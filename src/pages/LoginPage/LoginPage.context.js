59321020adf4c3cbafa3ab4cadc518a80c7551e3
Maksim <malcewdeveloper@yandex.ru>
2024-04-30 22:53:44 +0300

    feat: adds the login page

---
+++ src/pages/LoginPage/LoginPage.context.js
@@ -0,0 +1,21 @@
+export const LoginPageContext = {
+    title: 'Вход',
+    inputs: [
+        {
+            type: 'text',
+            placeholder: 'ivanivanov',
+            name: 'login',
+            label: 'Логин'
+
+        },
+        {
+            type: 'password',
+            placeholder: '••••••••••••',
+            name: 'password',
+            label: 'Пароль'
+        }
+    ],
+    button: {
+        text: 'Войти'
+    }
+}
