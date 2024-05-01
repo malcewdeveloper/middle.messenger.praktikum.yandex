612fc81b7a40d350073033ec526d17176647522a
Maksim <malcewdeveloper@yandex.ru>
2024-05-01 20:26:57 +0300

    refactor: adds pages structure and navigate func

--- src/index.js
+++ src/index.js
@@ -1,5 +1,58 @@
+import * as Components from './components';
+import * as UI from './ui';
+import * as Modules from './modules';
+import * as Pages from './pages';
+import Handlebars from 'handlebars';
+import './global.scss';
+
+
+const pages = {
+    '/': {
+        page: Pages.PreviewPage,
+        context: {}
+    },
+    '/main': {
+        page: Pages.ChatPage,
+        context: Pages.ChatPageContext
+    },
+    '/register': {
+        page: Pages.RegisterPage,
+        context: Pages.RegisterPageContext
+    },
+    '/login': {
+        page: Pages.LoginPage,
+        context: Pages.LoginPageContext
+    },
+    '/profile': {
+        page: Pages.ProfilePage,
+        context: Pages.ProfilePageContext
+    },
+    '/not-found': {
+        page: Pages.NotFound,
+        context: {}
+    },
+    '/error': {
+        page: Pages.Error,
+        context: Pages.ErrorContext
+    }
+}
+
+const navigate = () => {
+    if(pages[window.location.pathname]) {
+        const {page, context} = pages[window.location.pathname];
+
+        return Handlebars.compile(page)(context);
+    } else {
+        return Handlebars.compile(pages['/not-found'].page)();
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('app');
 
-    root.innerHTML = 'Hello world!'
+    Object.entries({...Components, ...UI, ...Modules}).forEach(([name, partial]) => {
+        Handlebars.registerPartial(name, partial);
+    });
+
+    root.innerHTML = navigate();
 });
