d67628b98cfcf603fd44a65fe05d414f0cc71ca1
Maksim <malcewdeveloper@yandex.ru>
2024-05-01 14:58:05 +0300

    feat: adds the error page

---
+++ src/pages/Error/index.js
@@ -0,0 +1,3 @@
+import './Error.scss';
+export * from './Error.context';
+export { default as Error } from './Error.hbs?raw';
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export * from './RegisterPage';
 export * from './LoginPage';
 export * from './PreviewPage';
 export * from './NotFound';
+export * from './Error';
