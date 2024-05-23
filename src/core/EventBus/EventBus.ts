806a0a658d434b57f81f919bcf022de0e4ca67fa
Maksim <malcewdeveloper@yandex.ru>
2024-05-23 15:06:05 +0300

    feat: adds event bus

---
+++ src/core/EventBus/EventBus.ts
@@ -0,0 +1,36 @@
+type EventName = string;
+
+type CallbackType = (...args: unknown[]) => void;
+
+export default class EventBus {
+    private readonly listeners: Record<EventName, CallbackType[]>;
+
+    constructor() {
+        this.listeners = {};
+    }
+
+    on(event: EventName, callback: CallbackType) {
+        if (!this.listeners[event]) {
+            this.listeners[event] = [];
+        }
+        this.listeners[event].push(callback);
+    }
+
+    off(event: EventName, callback: CallbackType) {
+        if (!this.listeners[event]) {
+            throw new Error(`Нет события: ${event}`);
+        }
+        this.listeners[event] = this.listeners[event].filter(
+            (listener) => listener !== callback,
+        );
+    }
+
+    emit(event: EventName, ...args: unknown[]) {
+        if (!this.listeners[event]) {
+            throw new Error(`Нет события: ${event}`);
+        }
+        this.listeners[event].forEach((listener) => {
+            listener(...args);
+        });
+    }
+}
