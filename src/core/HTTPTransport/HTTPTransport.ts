6765161eb4cbb0158e24bfadf342cf8719dd12a2
Maksim <malcewdeveloper@yandex.ru>
2024-06-16 22:00:59 +0300

    fix: adds queryString and FormData

--- src/core/HTTPTransport/HTTPTransport.ts
+++ src/core/HTTPTransport/HTTPTransport.ts
@@ -1,3 +1,5 @@
+import { queryString } from "../../utils";
+
 const METHODS = {
     GET: "GET",
     POST: "POST",
@@ -5,8 +7,10 @@ const METHODS = {
     DELETE: "DELETE",
 };
 
+const API_URL = "https://ya-praktikum.tech/api/v2";
+
 type Options = {
-    data?: Document | XMLHttpRequestBodyInit | null | undefined;
+    data?: object;
     headers?: Record<string, string>;
     method: keyof typeof METHODS;
     timeout?: number;
@@ -17,6 +21,12 @@ type OptionsWithoutMethod = Omit<Options, "method">;
 type HTTPResponse = Promise<XMLHttpRequest>;
 
 export default class HTTPTransport {
+    private _url?: string;
+
+    constructor(url?: string) {
+        this._url = `${API_URL}${url}`;
+    }
+
     get(url: string, options?: OptionsWithoutMethod): HTTPResponse {
         return this._request(
             url,
@@ -49,18 +59,23 @@ export default class HTTPTransport {
         );
     }
 
-    private _request(
+    private _request = (
         url: string,
         options: Options,
         timeout: number = 5000,
-    ): HTTPResponse {
+    ): HTTPResponse => {
         const { data, headers, method = "GET" } = options;
 
-        return new Promise(function (resolve, reject) {
+        return new Promise((resolve, reject) => {
             const xhr = new XMLHttpRequest();
             const isGet = method === METHODS.GET;
 
-            xhr.open(method, isGet ? `${url}${data}` : url);
+            xhr.open(
+                method,
+                isGet && data
+                    ? `${this._url}${url}${queryString(data)}`
+                    : `${this._url}${url}`,
+            );
 
             if (headers) {
                 Object.keys(headers).forEach((key) => {
@@ -80,8 +95,10 @@ export default class HTTPTransport {
             if (isGet || !data) {
                 xhr.send();
             } else {
-                xhr.send(data);
+                const requestData =
+                    data instanceof FormData ? data : JSON.stringify(data);
+                xhr.send(requestData);
             }
         });
-    }
+    };
 }
